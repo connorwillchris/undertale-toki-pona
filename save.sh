39f66862c0bc6abd4343207ed45174c8c3a03753
treeman0013 <connorwillchris@gmail.com>
2025-03-13 10:30:04 -0600

    alterations

--- save.sh
+++ save.sh
@@ -3,5 +3,5 @@ $HOME/Source/UndertaleModTool-0.6.1.0/UndertaleModCli/bin/Debug/net6.0/Undertale
     -o ./toki-pona.win -c gml_Script_textdata_en=./script.gml
 cp -rf ./toki-pona.win $HOME/.steam/root/steamapps/common/Undertale/assets/game.unx
 
-# Optional...removes all local savedata for a TRUE RESET.
+# removes all local savedata for a TRUE RESET.
 rm -rf $HOME/.config/UNDERTALE/undertale.ini
